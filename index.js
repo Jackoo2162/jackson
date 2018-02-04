18a182c5e440e8fc7ff15e4fb62af47e08186ace
Jackoo2162 <35918848+Jackoo2162@users.noreply.github.com>
2018-02-04 15:31:12 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+
+bot.on('message', message => {
+  if (message.content === 'shimmy shimmy ko ko bop I like that....') {
+   // message.reply('pong');
+    message.channel.sendMessage('I think I like that!');
+  }
+});
+
+bot.on("ready", () => {
+  bot.user.setPresence({game: {name:"DRINKING WITH LAY BOT AND DONGHAE BOT"}})
+  console.log('I have jackoo: {SERVERNAME}')
+})
+
+bot.on('ready', ()=>{
+  console.log(`Logged in as ${bot.user.tag}!`);
+  voiceChannel = bot.guilds.find('name', 'DREAMENERG3T1CER').channels.find('name', 'JACKSON');
+  voiceChannel.join().then(()=>{
+      console.log(`Connected to ${voiceChannel.name}!`);
+  });
+});
+
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+bot.login('NDA3NzM0MzcwODYzMTUzMTYy.DVGbcQ.t-ASTsRMwvnsQOwoYfvfQkajQAg');
